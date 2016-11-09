b6a149012d61185afaed7d12c6c772d51f31d07f
grantal <AlexV348@gmail.com>
2016-11-08 19:19:08 -0800

    addded threading

--- solitaire_s.c
+++ solitaire_s.c
@@ -9,6 +9,7 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define MAXLINE     8192
 #define CONNECTIONS 128
@@ -421,90 +422,37 @@ int moveOnto(card_t *card, card_t *onto, solitaire_t *S) {
   return FAILURE;
 }
 
-int main(int argc, char **argv) {
 
-  // 
-  // Make sure we've been given a port to listen on.
-  //
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
-    exit(0);
-  }
+typedef struct _client_t {
+    int id;
+    int connection;
+    struct sockaddr_in address;
+} client_t;
+
+void *solitaire_session(void *ci) {
+  client_t *client = (client_t *)ci;
+
+  int id = client->id;
+  int connfd = client->connection;
   
   //
-  // Open a socket to listen for client connections.
+  // Report the client that connected.
   //
-  int listenfd;
-  if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    fprintf(stderr,"SOCKET creation failed.\n");
-    exit(-1);
+  struct hostent *hostp;
+  if ((hostp = gethostbyaddr((const char *)&client->address.sin_addr.s_addr, 
+			     sizeof(struct in_addr), 
+			     AF_INET)) == NULL) {
+    fprintf(stderr, "GETHOSTBYADDR failed for client %d.",id);
   }
   
-  //
-  // Build the service's info into a (struct sockaddr_in).
-  //
-  int port = atoi(argv[1]);
-  struct sockaddr_in serveraddr;
-  bzero((char *) &serveraddr, sizeof(struct sockaddr_in));
-  serveraddr.sin_family = AF_INET; 
-  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-  serveraddr.sin_port = htons((unsigned short)port); 
-
-  //
-  // Bind that socket to a port.
-  //
-  if (bind(listenfd, (struct sockaddr *)&serveraddr, sizeof(struct sockaddr)) < 0) {
-    fprintf(stderr,"BIND failed.\n");
-    exit(-1);
-  }
-
-  //
-  // Listen for client connections on that socket.
-  //
-  if (listen(listenfd, CONNECTIONS) < 0) {
-    fprintf(stderr,"LISTEN failed.\n");
-    exit(-1);
-  }
+  printf("Accepted connection from client %d %s (%s)\n", 
+	 id, 
+	 hostp->h_name, 
+	 inet_ntoa(client->address.sin_addr));
   
-  fprintf(stderr,"Solitaire server listening on port %d...\n",port);
-  
-  int clients = 0;
-
   //
-  // Handle client sessions.
+  // Play Solitaire
   //
-  while (1) {
-    
-    //
-    // Accept a connection from a client, get a file descriptor for communicating
-    // with the client.
-    //
-    socklen_t acceptlen = sizeof(struct sockaddr_in);
-    struct sockaddr_in clientaddr;
-    int connfd;
-    if ((connfd = accept(listenfd, (struct sockaddr *)&clientaddr, &acceptlen)) < 0) {
-      fprintf(stderr,"ACCEPT failed.\n");
-      exit(-1);
-    }
-
-
-    //
-    // Report the client that connected.
-    //
-    struct hostent *hostp;
-    if ((hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
-			       sizeof(struct in_addr), 
-			       AF_INET)) == NULL) {
-      fprintf(stderr, "GETHOSTBYADDR failed.");
-    }
-    
-    clients = clients + 1;
-    printf("Accepted connection from client %d %s (%s)\n", 
-	   clients, 
-	   hostp->h_name, 
-	   inet_ntoa(clientaddr.sin_addr));
-
-    // play solitaire
     
     //make seed and send it
     struct timeval tp; 
@@ -580,10 +528,89 @@ int main(int argc, char **argv) {
         printf("%s\n", response);
     }
       
+  //
+  // Close the client connection.
+  //
+  close(connfd);
+
+  return NULL;
+    
+}
+
+int main(int argc, char **argv) {
+
+  // 
+  // Make sure we've been given a port to listen on.
+  //
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(0);
+  }
+  
+  //
+  // Open a socket to listen for client connections.
+  //
+  int listenfd;
+  if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    fprintf(stderr,"SOCKET creation failed.\n");
+    exit(-1);
+  }
+  
+  //
+  // Build the service's info into a (struct sockaddr_in).
+  //
+  int port = atoi(argv[1]);
+  struct sockaddr_in serveraddr;
+  bzero((char *) &serveraddr, sizeof(struct sockaddr_in));
+  serveraddr.sin_family = AF_INET; 
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+  serveraddr.sin_port = htons((unsigned short)port); 
+
+  //
+  // Bind that socket to a port.
+  //
+  if (bind(listenfd, (struct sockaddr *)&serveraddr, sizeof(struct sockaddr)) < 0) {
+    fprintf(stderr,"BIND failed.\n");
+    exit(-1);
+  }
+
+  //
+  // Listen for client connections on that socket.
+  //
+  if (listen(listenfd, CONNECTIONS) < 0) {
+    fprintf(stderr,"LISTEN failed.\n");
+    exit(-1);
+  }
+  
+  fprintf(stderr,"Solitaire server listening on port %d...\n",port);
+  
+  int clients = 0;
+
+  //
+  // Handle client sessions.
+  //
+  while (1) {
     //
-    // Close the client connection.
+    // Build a client's profile to send to a handler thread.
     //
-    close(connfd);
+    client_t *client = (client_t *)malloc(sizeof(client_t));
+
+    //
+    // Accept a connection from a client, get a file descriptor for communicating
+    // with the client.
+    //
+    client->id = clients;
+    socklen_t acceptlen = sizeof(struct sockaddr_in);
+    if ((client->connection = accept(listenfd, (struct sockaddr *)&client->address, &acceptlen)) < 0) {
+      fprintf(stderr,"ACCEPT failed.\n");
+      exit(-1);
+    }
+
+    //
+    // Create a thread to handle the client.
+    //
+    pthread_t tid;
+    pthread_create(&tid,NULL,solitaire_session,(void *)client);
   }
 
   close(listenfd);
